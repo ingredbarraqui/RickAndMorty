6eff07b6735bb1271e872cdeeef373960fbde8cd
ingredbarraqui <ingred.vferreira@gmail.com>
2025-05-15 08:20:09 -0300

    feat: ApolloClient main.js

--- src/main.js
+++ src/main.js
@@ -6,9 +6,14 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import apolloClient from './api/client'
+import { DefaultApolloClient } from '@vue/apollo-composable'
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+app.provide(DefaultApolloClient, apolloClient)
 
 app.mount('#app')
